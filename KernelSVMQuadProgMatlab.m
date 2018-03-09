05c3c0e3fbe773d5006e42f2538d440475037e8a
jaylohokare <jaylohokare@gmail.com>
2018-03-09 16:02:50 -0500

    Added MALTAB quadprog interface

--- KernelSVMQuadProgMatlab.m
+++ KernelSVMQuadProgMatlab.m
@@ -1,1 +1,23 @@
-load ..\q3_1_data.mat
+load q3_1_data.mat
+
+C = 10;
+
+[w,b,a,obj] = SVM(trD,trLb,C);
+
+val = valD'*w+b;
+
+
+
+
+
+
+function [w,b,a,obj]=SVM(X,Y,C)
+opts.Display='off';
+[alpha,obj,flag]=quadprog(X'*X,-ones(size(Y,1),1),[],[],Y',0,zeros(size(Y,1),1),C*ones(size(Y,1),1),[],opts);
+obj=-obj;
+[~,freesvs]=max(min(alpha,C-alpha));
+b=Y(freesvs)-K(freesvs,:)*diag(Y)*alpha;
+%[a,b,obj]=KernelSVM2(K,Y,C);
+w=X*diag(Y)*a;
+end
+
