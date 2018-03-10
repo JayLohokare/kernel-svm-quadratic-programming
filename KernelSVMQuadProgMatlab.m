4b35922cba3fc84ebeae911544b8417362f9d48c
jaylohokare <jaylohokare@gmail.com>
2018-03-10 18:53:05 -0500

    Final MATLAB commit

--- KernelSVMQuadProgMatlab.m
+++ KernelSVMQuadProgMatlab.m
@@ -1,23 +1,48 @@
 load q3_1_data.mat
-
-C = 10;
-
+C = 0.1;
 [w,b,a,obj] = SVM(trD,trLb,C);
-
 val = valD'*w+b;
 
+truePos=sum((val>=0)&(valLb>0));
+falseNeg=sum((val<0)&(valLb>0));
+trueNeg=sum((val<0)&(valLb<0));
+falsePos=sum((val>=0)&(valLb<0));
+
+accuracy=(truePos+trueNeg)/length(valLb);
+
+fprintf('Value of C %f:\n',C);
+fprintf('The model accuracy is %.2f\n', accuracy*100);
+fprintf('Objective value is %.2f\n', obj);
+fprintf('The number of SVs is %.2f\n', sum(a>1e-1));
+
+fprintf('Confusion matrix is \n');
+fprintf('%d %d\n%d %d\n',truePos,falsePos,falseNeg,trueNeg);
 
 
+C = 10;
+[w,b,a,obj] = SVM(trD,trLb,C);
+val = valD'*w+b;
 
+truePos=sum((val>=0)&(valLb>0));
+falseNeg=sum((val<0)&(valLb>0));
+trueNeg=sum((val<0)&(valLb<0));
+falsePos=sum((val>=0)&(valLb<0));
 
+accuracy=(truePos+trueNeg)/length(valLb);
 
+fprintf('Value of C %f:\n',C);
+fprintf('The model accuracy is %.2f\n', accuracy*100);
+fprintf('Objective value is %.2f\n', obj);
+fprintf('The number of SVs is %.2f\n', sum(a>1e-1));
+
+fprintf('Confusion matrix is \n');
+fprintf('%d %d\n%d %d\n',truePos,falsePos,falseNeg,trueNeg);
 function [w,b,a,obj]=SVM(X,Y,C)
-opts.Display='off';
-[alpha,obj,flag]=quadprog(X'*X,-ones(size(Y,1),1),[],[],Y',0,zeros(size(Y,1),1),C*ones(size(Y,1),1),[],opts);
+K=X'*X;
+options.Display='off';
+[a,obj,flag]=quadprog(diag(Y)*K*diag(Y),-ones(size(Y,1),1),[],[],Y',0,zeros(size(Y,1),1),C*ones(size(Y,1),1),[],options);
 obj=-obj;
-[~,freesvs]=max(min(alpha,C-alpha));
-b=Y(freesvs)-K(freesvs,:)*diag(Y)*alpha;
-%[a,b,obj]=KernelSVM2(K,Y,C);
+[~,sv]=max(min(a,C-a));
+b=Y(sv)-K(sv,:)*diag(Y)*a;
 w=X*diag(Y)*a;
 end
-
